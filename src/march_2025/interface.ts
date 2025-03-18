a92b1a19deaeeaafa05d5db8bc408f902950bd49
Soham <mail.sohamshinde@gmail.com>
2025-03-18 15:41:46 -0400

    Interface Practice

---
+++ src/march_2025/interface.ts
@@ -0,0 +1,32 @@
+//Interface
+interface IShape {
+    perimeter(perimeterParam: number): number
+    area(areaParam: number): number
+    sides(sidesParam: number): number
+}
+//Abstract Class
+abstract class Shape implements IShape {
+    perimeter(perimeterParam: number): number {
+        return perimeterParam
+    }
+
+    area(areaParam: number): number {
+        return areaParam
+    }
+
+    abstract sides(sidesParam: number): number
+}
+//Concrete Class
+class Circle implements IShape {
+    perimeter(perimeterParam: number): number {
+        return perimeterParam
+    }
+
+    area(areaParam: number): number {
+        return areaParam
+    }
+
+    sides(sidesParam: number): number {
+        return sidesParam
+    }
+}
