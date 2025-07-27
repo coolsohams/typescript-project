4d2fef5c428094f9d508baa07f4d8c4488aae22d
sohams <mail.sohamshinde@gmail.com>
2025-07-27 17:11:14 -0400

    Working sqrt code and deleted usless file.

--- src/july_2025/sqrt-and-squares.ts
+++ src/july_2025/sqrt-and-squares.ts
@@ -29,38 +29,73 @@ class SquareCalculator {
         return squareForCurrentPair; 
     }
 
+    isPerfectSquare(sqrtNumber: number) : boolean {
+        let squaresArray: Array<number> = this.findSquares(sqrtNumber);
+        for(let i: number = 0; i <= sqrtNumber; i++) {
+            if(squaresArray[i] == sqrtNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    findSecondDigit(firstPartOfDivisor: number, currentDivident: number) : number {
+        // Find the second digit of the divisor
+        let secondPartDigit: number = 0;
+
+        let result = Number.parseInt((firstPartOfDivisor.toString() +  secondPartDigit.toString())) * secondPartDigit;
+        while(!(result > currentDivident)) {
+            secondPartDigit++;
+            result = Number.parseInt((firstPartOfDivisor.toString() +  secondPartDigit.toString())) * secondPartDigit;
+        }
+        
+        return result > currentDivident ? secondPartDigit - 1 : secondPartDigit;
+    }
+
     findSqrt(sqrtNumber: number) {
         // Calculate squareroot of a non-perfect sqaure number
         let pairsArray: Array<number> = Utility.createPairs(sqrtNumber);
+        // if (!this.isPerfectSquare(sqrtNumber)) {
+        //     pairsArray.push(0)
+        // }
+
         let quotient: number = 0;
         let currentRemainder: number = 0;
         let overAllQuotient: number = 0;
         let divisor = this.findSquareRootOfNumber(pairsArray[0]);
+        let currentDivident: number = 0;
+        let firstPartOfDivisor: number = 0;
+        let secondPartDigit: number = 0;
 
         for(let i: number = 0; i <= pairsArray.length - 1; i++) {
             let currentPair: number = pairsArray[i];
-            let currentDivident: number = Number.parseInt(currentRemainder.toString() + currentPair.toString());
+            currentDivident = Number.parseInt(currentRemainder.toString() + currentPair.toString());
             quotient = Utility.getQuotient(divisor, currentDivident);
             overAllQuotient = Number.parseInt(overAllQuotient.toString() + quotient.toString()) 
             currentRemainder = currentDivident % divisor;
 
-            // Add current divisor and last digit of the quotient
-            let firstPartOfDivisor: number = divisor + Number.parseInt(overAllQuotient.toString()[overAllQuotient.toString().length - 1]);
-           
-            // Find the second digit of the divisor
-            let secondPartDigit: number = 0;
-
-            while(firstPartOfDivisor * secondPartDigit <= currentDivident) {
-                secondPartDigit++;
+            firstPartOfDivisor = divisor + Number.parseInt(overAllQuotient.toString()[overAllQuotient.toString().length - 1]);
+            
+            
+            if(pairsArray.length - 1 != i) {
+                currentDivident = Number.parseInt(currentRemainder.toString() + pairsArray[i+1].toString());
+                secondPartDigit = this.findSecondDigit(firstPartOfDivisor, currentDivident);
                 divisor = Number.parseInt(firstPartOfDivisor.toString() + secondPartDigit.toString());
             }
         } 
-        
+
+        if(currentRemainder != 0) {
+            // Adding a pair of 0's by multiplying by 100.
+            currentDivident = currentRemainder * 100;
+            secondPartDigit = this.findSecondDigit(firstPartOfDivisor, currentDivident);
+            overAllQuotient = overAllQuotient + Number.parseFloat("." + secondPartDigit.toString());
+        }
+
         return overAllQuotient;
     }
 }
 
 let sqrtObject: SquareCalculator = new SquareCalculator();  
 
-console.log(sqrtObject.findSqrt(12673));
+console.log(sqrtObject.findSqrt(676767));
 
--- src/july_2025/teaching.ts
+++
@@ -1,3 +0,0 @@
-var myVariable: Array<any> = [234532423,"sdgfhdsgfhjdsgfds",true,false,];
-
-console.log(myVariable[0]);
