fdd963f6b97ee9d934b2d04779ef27e4ab827fd1
sohams <mail.sohamshinde@gmail.com>
2025-12-01 19:39:13 -0500

    Done wordle game!

--- src/november_2025/wordle-game-html-version.ts
+++ src/november_2025/wordle-game-html-version.ts
@@ -1,32 +1,50 @@
+enum Color {
+    RED = 'bg-red-600',
+    GREEN = 'bg-green-600',
+    YELLOW = 'bg-yellow-300',
+    EMPTY = ''
+}
+
+class GridElement {
+    value: string = '';
+    color: Color = Color.EMPTY;
+    constructor(value?: string, color?: Color) {
+        this.value = value!;
+        this.color = color!;
+    }
+}
+
+let emptyGridElemt: GridElement = new GridElement('', Color.EMPTY);
 class WordleGrid {
-    elements: Array<Array<string>> = [
-        ['', '', '', '', ''],
-        ['', '', '', '', ''],
-        ['', '', '', '', ''],
-        ['', '', '', '', ''],
-        ['', '', '', '', '']
+    guessCounter: number = 0;
+    elements: Array<Array<GridElement>> = [
+        [emptyGridElemt, emptyGridElemt, emptyGridElemt, emptyGridElemt, emptyGridElemt],
+        [emptyGridElemt, emptyGridElemt, emptyGridElemt, emptyGridElemt, emptyGridElemt],
+        [emptyGridElemt, emptyGridElemt, emptyGridElemt, emptyGridElemt, emptyGridElemt],
+        [emptyGridElemt, emptyGridElemt, emptyGridElemt, emptyGridElemt, emptyGridElemt],
+        [emptyGridElemt, emptyGridElemt, emptyGridElemt, emptyGridElemt, emptyGridElemt]
     ];
     
-    constructor(elements?:  Array<Array<string>>) {
+    constructor(elements?:  Array<Array<GridElement>>) {
         if(elements) {
             this.elements = elements;
         }
     }
     
-    drawGrid(document: Document, divId: string, className: string) {
+    drawGrid(document: Document, divId: string) {
         let gridHtml: string = '';
         for(let i: number = 0; i < this.elements.length; i++) {
             
-            let row: Array<string> = this.elements[i];
+            let row: Array<GridElement> = this.elements[i];
             let rowHtml: string =  ``;
             for(let j: number = 0; j < row.length; j++) {
-                let currentDivIdCoordinate: string = j.toString() + '_' + i.toString() 
-                rowHtml = rowHtml + this.createGridItem(className, row[j], currentDivIdCoordinate);
+                let currentDivIdCoordinate: string = i.toString() + '_' + j.toString() 
+                rowHtml = rowHtml + this.createGridItem(row[j].color, row[j].value, currentDivIdCoordinate);
             }
             
             gridHtml =  gridHtml + `
             <!--Row 1 Div-->
-            <div class="space-y-2 p-1">
+            <div class="gap-2 p-2 flex flex-row">
                 ${rowHtml}
             </div>`
         }
@@ -38,74 +56,58 @@ class WordleGrid {
     }
     
     createGridItem(className: string, value: string, divId: string): string {
-        return `<div id="${divId}" class="border border-b-emerald-700 rounded-sm p-2 ${className}">${value}</div>`;
-    }
-}
-
-
-class WordleGameHTML {
-    private prompts = require('prompt-sync')({ sigint: true }); // Enables Ctrl+C to exit;
-    startGame() {
-        let guessingWord: string = this.prompts('Write a 5 letter word: ');
-        guessingWord = guessingWord.toUpperCase();
-        if(guessingWord.length != 5){
-            console.log("The word you chose is not 5 letters! Game will restart and you can write a 5 letter word.");
-            this.startGame()
-        } else {
-            let didPlayerWin: boolean = false;
-            for(let i = 0; i < 5 && !didPlayerWin; i++) {
-                let guessedWord: string = this.prompts("Guess a word: ");
-                guessedWord = guessedWord.toUpperCase();
-            }
-        }
+        return `<div id="${divId}" class="border border-b-emerald-700 rounded-sm p-5 max-h-4 max-w-4 flex items-center ${className}">${value}</div>`;
     }
 }
 
 let guessingWord: string = '';
 let wordleGridObject: WordleGrid = new WordleGrid();
 
-wordleGridObject.drawGrid(document, 'displayDiv', 'p-5 m-2');
+wordleGridObject.drawGrid(document, 'displayDiv');
 function submitWord() {
     const guessingWordInputValue: string = (document.getElementById('wordInput') as HTMLInputElement).value;
     if(guessingWordInputValue.length != 5) {
         alert('Choose a 5 letter word!')
     } else {
         alert('Word sucessfully chosen!')
-        guessingWord =  guessingWordInputValue;
+        guessingWord =  guessingWordInputValue.toUpperCase();
     }
 }
 
 function submitGuessWordle() {
-    let currentGuess: string = '';
-    let wordleGridObject: WordleGrid = new WordleGrid();
-    const guessedWordInputValue: string = (document.getElementById('guessInput') as HTMLInputElement).value;
+    let guessedWordInputValue: string = (document.getElementById('guessInput') as HTMLInputElement).value.toUpperCase();
+    
     if(guessedWordInputValue.length != 5) {
         alert('Choose a 5 letter word!')
     } else {
         const numberOfGuessesLeftDiv = (document.getElementById('numberOfGuessesLeftDisplayDiv') as HTMLDivElement);
-        numberOfGuessesLeftDiv.innerText = (Number.parseInt(numberOfGuessesLeftDiv.innerText) + 1).toString();
-        currentGuess = guessedWordInputValue
-        for(let i = 0; i < 5; i++) {
-            if(guessingWord == currentGuess) {
-                alert('Nice, You found the word!');
-                for(let i = 0; i < 5; i++) {
-                    const currentDiv = (document.getElementById((Number.parseInt(numberOfGuessesLeftDiv.innerText) - 1).toString() + '_' + i.toString()) as HTMLDivElement);
-                    currentDiv.innerHTML = `<div class="border border-b-emerald-700 rounded-sm p-2 bg-green-600">${currentGuess[i]}</div>`;
-                }
-                break;
-            } else if (currentGuess[i] == guessingWord[i]) {
-                const currentDiv = (document.getElementById((Number.parseInt(numberOfGuessesLeftDiv.innerText) - 1).toString() + '_' + i.toString()) as HTMLDivElement);
-                currentDiv.innerHTML = `<div class="border border-b-emerald-700 rounded-sm p-2 bg-green-600">${currentGuess[i]}</div>`;
-                // wordleGridObject.elements[i][Number.parseInt(numberOfGuessesLeftDiv.innerText)] = guessingWord[i]
-                // wordleGridObject.drawGrid(document, 'displayDiv', 'p-5 m-2');
-            } else if (currentGuess[i] == guessingWord[0] || currentGuess[i] == guessingWord[1] || currentGuess[i] == guessingWord[2] || currentGuess[i] == guessingWord[3] || currentGuess[i] == guessingWord[4]) {
-                const currentDiv = (document.getElementById((Number.parseInt(numberOfGuessesLeftDiv.innerText) - 1).toString() + '_' + i.toString()) as HTMLDivElement);
-                currentDiv.innerHTML = `<div class="border border-b-emerald-700 rounded-sm p-2 bg-yellow-300">${currentGuess[i]}</div>`;
-            } else if (currentGuess[i] != guessingWord[0] || currentGuess[i] != guessingWord[1] || currentGuess[i] != guessingWord[2] || currentGuess[i] != guessingWord[3] || currentGuess[i] != guessingWord[4]) {
-                const currentDiv = (document.getElementById((Number.parseInt(numberOfGuessesLeftDiv.innerText) - 1).toString() + '_' + i.toString()) as HTMLDivElement);
-                currentDiv.innerHTML = `<div class="border border-b-emerald-700 rounded-sm p-2 bg-red-600">${currentGuess[i]}</div>`;
+        numberOfGuessesLeftDiv.innerText = (wordleGridObject.guessCounter + 1).toString();
+        if(guessingWord == guessedWordInputValue) {
+            alert('Nice, You found the word!');
+            const array: Array<GridElement> = [];
+            for(let i = 0; i < 5; i++) {
+                const gridElement: GridElement = new GridElement(guessedWordInputValue[i], Color.GREEN);
+                array.push(gridElement);
             }
-        }
+            wordleGridObject.elements[wordleGridObject.guessCounter] = array;
+        }  else {
+            const array: Array<GridElement> = [];
+            for(let i = 0; i < 5; i++) {
+                if (guessedWordInputValue[i] == guessingWord[i]) {
+                    const gridElement: GridElement = new GridElement(guessedWordInputValue[i], Color.GREEN);
+                    array.push(gridElement);
+                } else if (guessedWordInputValue[i] == guessingWord[0] || guessedWordInputValue[i] == guessingWord[1] || guessedWordInputValue[i] == guessingWord[2] || guessedWordInputValue[i] == guessingWord[3] || guessedWordInputValue[i] == guessingWord[4]) {
+                    const gridElement: GridElement = new GridElement(guessedWordInputValue[i], Color.YELLOW);
+                    array.push(gridElement);
+                } else if (guessedWordInputValue[i] != guessingWord[0] || guessedWordInputValue[i] != guessingWord[1] || guessedWordInputValue[i] != guessingWord[2] || guessedWordInputValue[i] != guessingWord[3] || guessedWordInputValue[i] != guessingWord[4]) {
+                    const gridElement: GridElement = new GridElement(guessedWordInputValue[i], Color.RED);
+                    array.push(gridElement);
+                }
+            }
+            wordleGridObject.elements[wordleGridObject.guessCounter] = array;
+        }      
+        wordleGridObject.drawGrid(document, 'displayDiv');
+        wordleGridObject.guessCounter++;
     }
 }
 
